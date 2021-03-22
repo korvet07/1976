bf6e92e0a071404c5dd0abe66bc5ce43a50f0ac4
Anatoly U <korvet07@yandex.ru>
2021-03-22 12:36:00 +0300

    mod_yaboot_AND_position

--- Bots/bot_for_yandex.js
+++ Bots/bot_for_yandex.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.yandex.ru/*
+// @match        https://yandex.ru/*
 // @grant        none
 // ==/UserScript==
 let keywords = ["Как звучит флейта", "Валторна", "Тромбон", "Кларнет", "Фагот", "Гобой", "Саксофон"];
