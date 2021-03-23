8244bb1c4d22e637b9c58ddb6643e73ac133ddbe
Anatoly U <korvet07@yandex.ru>
2021-03-23 08:43:19 +0300

    for_offcet

--- coffee_machine/coffee_machine.php
+++ coffee_machine/coffee_machine.php
@@ -1,70 +1,96 @@
+
 <!doctype html>
-<html lang="en">
-  <head>
+<html lang="ru">
+<head>
+    <title>Кофемашина</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- FontAwesome -->
     <script src="https://kit.fontawesome.com/750282b7c8.js" crossorigin="anonymous"></script>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link href="css/style.css" rel="stylesheet">
-    <title>Кофе машина</title>
-  </head>
-  <body>
-    <div id="lock" hidden></div>
-    <div class="container rounded mt-5">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="coffee-btn-group my-3">
-            <div class="coffee-btn rounded-circle" onclick="getCoffee('Латте',42);"></div>
-            <span>Латте - 42</span>
-          </div>
-          <div class="coffee-btn-group my-3">
-            <div class="coffee-btn rounded-circle" onclick="getCoffee('Американо',47);"></div>
-            <span>Американо - 47</span>
-          </div>
-          <div class="coffee-btn-group my-3">
-            <div class="coffee-btn rounded-circle" onclick="getCoffee('Эспрессо',61);"></div>
-            <span>Эспрессо - 61</span>
-          </div>
-          <div class="coffee-btn-group my-3">
-            <div class="coffee-btn rounded-circle" onclick="getCoffee('Капучино',58);"></div>
-            <span>Капучино - 58</span>
-          </div>
-          <div class="coffee-btn-group my-3">
-            <div class="coffee-btn rounded-circle" onclick="getCoffee('Лунго',44);"></div>
-            <span>Лунго - 44</span>
-          </div>
-          
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div id="block_screen" hidden></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 mt-5">
+                <h1 class="text-center">
+                    <a href="index.php">
+                        <img src="img/coffee_bg.png" alt="Кофе" width="100">
+                        Кофемашина
+                    </a>
+                </h1>
+            </div>
         </div>
-        <div class="col-md-6">
-          <div class="row my-3">
-            <div class="col-sm-6 text-center">
-              <div id="display" class="p-3 text-center">
-                <p id="displayInfo">Внесите деньги и закажите напиток</p>
-                <p><i class="fas fa-money-bill-alt"></i> Баланс: <span id="displayBalance">0</span>руб.</p>
-                <div class="progress" style="background-color:navy;">
-                  <div class="progress-bar bg-white" role="progressbar" style="width: 0%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <ul class="coffee_btns">
+                    <li>
+                        <div class="coffee_btn" onclick="getCoffee('Латте',42)">
+                            <i class="fas fa-coffee"></i>
+                        </div>
+                        Латте - 42
+                    </li>
+                    <li>
+                        <div class="coffee_btn" onclick="getCoffee('Американо',47)">
+                            <i class="fas fa-coffee"></i>
+                        </div>
+                        Американо - 47
+                    </li>
+                    <li>
+                        <div class="coffee_btn" onclick="getCoffee('Эспрессо',61)">
+                            <i class="fas fa-coffee"></i>
+                        </div>
+                        Эспрессо - 61
+                    </li>
+                    <li>
+                        <div class="coffee_btn" onclick="getCoffee('Капучино',58)">
+                            <i class="fas fa-coffee"></i>
+                        </div>
+                        Капучино - 58
+                    </li>
+                    <li>
+                        <div class="coffee_btn" onclick="getCoffee('Лунго',44)">
+                            <i class="fas fa-coffee"></i>
+                        </div>
+                        Лунго - 44
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-6 coffee_bg">
+                <div class="row my-3">
+                    <div class="col-sm-6 text-center">
+                        <div id="display" class="mt-3">
+                            <div id="display_info">Внесите деньги и закажите напиток</div>
+                            <div id="display_balance"></div>
+                            <div class="progress mx-2 mt-1" id="coffee_progress" hidden>
+                                <div class="progress-bar" role="progressbar" style="width: 0%"></div>
+                            </div>
+                        </div>
+                        <a id="get_change_btn" class="my-3" href="#" onclick="getChange()"><i class="fas fa-coins"></i>Выдать сдачу</a>
+                        <img src="img/cup.png" alt="Чашка кофе" width="300" id="coffee_cup" hidden>
+                    </div>
+                    <div class="col-sm-6 text-center my-3">
+                        <input type="text" id="moneyField" hidden>
+                        <img src="img/bill_acc.png" alt="Купюроприёмник" class="mt-2" id="bill_acc">
+                        <div id="change_box" class="mt-3"></div>
+                    </div>
                 </div>
-              </div>
-              <button class="btn btn-lg btn-light my-3" onclick="getChange(money.value)"><i class="fas fa-hand-holding-usd"></i> Выдать сдачу</button>
             </div>
-            <div class="col-sm-6 text-center">
-              <input hidden id="money" type="text">
-              <img id="bill_acc" src="img/bill_acc.png" alt="">
-              <div id="change_box" class="mx-auto mt-3"></div>
-            </div>
-          </div>
         </div>
-      </div>
     </div>
     <div class="container mt-5">
-      <img src="img/50rub.jpg" data-value="50" alt="50 рублей">
-      <img src="img/100rub.jpg" data-value="100" alt="100 рублей">
-      <img src="img/500rub.jpg" data-value="500" alt="500 рублей">
+            <img src="img/50rub.jpg" alt="50 рублей" data-value="50">
+            <img src="img/100rub.jpg" alt="100 рублей" data-value="100">
+            <img src="img/500rub.jpg" alt="500 рублей" data-value="500">
     </div>
-    <script src="js/script.js"></script>
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-  </body>
+    <script src="js/script.js"></script>
+</body>
 </html>
